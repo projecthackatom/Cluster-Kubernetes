79ad0c09ef5c32ae3c45c9935d517bab8041a622
projecthackatom <projecthackatom@gmail.com>
2025-06-19 04:21:23 +0000

    version 1

---
+++ microservicio​/server.js
@@ -0,0 +1,11 @@
+const express = require('express');​
+const app = express();​
+const PORT = 8080;​
+​
+app.get('/', (req, res) => {​
+res.send('¡Hola desde mi microservicio!');​
+});​
+​
+app.listen(PORT, () => {​
+console.log(`Servidor corriendo en http://localhost:${PORT}`);​
+});
